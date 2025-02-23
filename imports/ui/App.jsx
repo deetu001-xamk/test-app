c067f0f86a5485c93da55ea2eca63a71b96c7e69
Eetu Turtiainen <deetu001@edu.xamk.fi>
2025-02-23 14:21:52 +0200

    Kirjautumis sivu

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React, {useState} from 'react';
+import { LoginPage } from './LoginPage';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+
+        <div>
+                    
+            { false ? (
+                <>
+                    <h1 className="text-3xl">Welcome to Meteor!</h1>
+                </>
+
+
+
+            ) : (
+                
+
+                <LoginPage />
+
+
+            )}
+
+        
+        
+
+        </div>
+
+    )
+};
